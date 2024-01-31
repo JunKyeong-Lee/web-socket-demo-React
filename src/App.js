b18d7f152beaa38664cfcd60fce3fc2c639a6969
JunKyeong-Lee <worldreaming@gmail.com>
2024-01-31 14:39:29 +0900

    websocket react 부분 데모 완료

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { Client } from '@stomp/stompjs';
 
-function App() {
+const App = () => {
+  const [messages, setMessages] = useState([]);
+  const [messageInput, setMessageInput] = useState('');
+  const [stompClient, setStompClient] = useState(null);
+
+
+
+  useEffect(() => {
+    const connectAndSubscribe = () => {
+      const socket = new WebSocket('ws://localhost:8080/chat'); // WebSocket 서버 주소
+      const stomp = new Client();
+
+      stomp.webSocketFactory = () => socket;
+      stomp.activate();
+
+      stomp.onConnect = () => {
+        console.log('Connected to WebSocket');
+        setStompClient(stomp);
+      };
+    };
+
+    connectAndSubscribe();
+
+    return () => {
+      stompClient && stompClient.deactivate();
+    };
+  }, []);
+  useEffect(() => {
+    // 특정 주제("/topic/public/rooms/{roomId}")를 구독
+    stompClient && stompClient.subscribe('/topic/public/rooms/1', (message) => {
+      const receivedMessage = JSON.parse(message.body);
+      console.log('수신시작 >>> message:', receivedMessage);
+      setMessages((prevMessages) => [...prevMessages, receivedMessage]);
+      console.log('>>> 수신완료');
+    });
+  }, [stompClient])
+
+  useEffect(() => {
+    console.log('현재 messages:', messages);
+
+  }, [messages])
+
+
+  const sendMessage = () => {
+    // 메시지를 WebSocket으로 보내기
+    const message = {
+      from: 'React User',
+      text: messageInput,
+    };
+    console.log('-----------------------------------------------');
+    console.log('전송시작 >>> message:', message);
+
+    stompClient && stompClient.publish({
+      destination: '/app/chat/rooms/1/send',
+      body: JSON.stringify(message),
+    });
+    console.log('>>> 전송완료');
+    setMessageInput('');
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        {messages.map((msg, index) => (
+          <div key={index}>
+            <strong>{msg.from}:</strong> {msg.text}
+          </div>
+        ))}
+      </div>
+      <input
+        type="text"
+        value={messageInput}
+        onChange={(e) => setMessageInput(e.target.value)}
+      />
+      <button onClick={sendMessage}>Send</button>
     </div>
   );
-}
+};
 
 export default App;
